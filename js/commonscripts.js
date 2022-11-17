052072692499d30bd3e761b61128d589fcc48446
ch-wd <charan2711@gmail.com>
2022-11-17 11:11:52 +0530

    added multiple products

---
+++ js/commonscripts.js
@@ -0,0 +1,10 @@
+var dropList = document.querySelector('.drop-down');
+
+
+function displayDropDown(){
+	dropList.style.display = "block";
+}
+
+function hideDropDown(){
+	dropList.style.display = "none";
+}
